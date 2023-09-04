2f03aeba61f1ffdd9be9ce1503628527a06e0536
HusseinHany <hhany3997@gmail.com>
2023-09-04 15:54:40 +0300

    Write basic structure of Todo & Project class

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
-console.log("Hello, Todo List!");
+// has title, description, dueDate, priority, notes, completed;
+class Todo {
+    constructor(title, description, dueDate, priority, notes, completed) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.notes = notes;
+        this.completed = completed;
+    }
+
+    markComplete() {
+        this.completed = true;
+    }
+}
+
+// project class
+class Project {
+    constructor(name) {
+        this.name = name;
+        this.todos = [];
+    }
+
+    addTodo(todo) {
+        this.todos.push(todo);
+    }
+
+    removeTodo(todo) {
+        const index = this.todos.indexOf(todo);
+        if (index !== -1) {
+            this.todos.splice(index, 1);
+        }
+    }
+}
